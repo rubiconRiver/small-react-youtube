c7417474c0eb3ec6867997be302413a28f2bb14f
Garett Gelsinger <garett.gelsinger@gmail.com>
2018-07-09 13:42:33 -0600

    finished skeleton search function

--- src/index.js
+++ src/index.js
@@ -17,20 +17,23 @@ class App extends Component {
       videos: [],
       selectedVideo: null
     };
+    this.videoSearch("pokemon");
+  }
 
-    YTSearch({ key: API_KEY, term: "surfboards" }, videos => {
+  videoSearch = term => {
+    YTSearch({ key: API_KEY, term: term }, videos => {
       //this.setstate({videos: videos});
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
       });
     });
-  }
+  };
 
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
