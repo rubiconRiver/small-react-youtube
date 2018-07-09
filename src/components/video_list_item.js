635c12c4b00d1d92a1e3de95042db3e5b7fab4ab
Garett Gelsinger <garett.gelsinger@gmail.com>
2018-07-09 11:46:11 -0600

    added video selection

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 //instead of (props), const video = props.video, this line will do the same
-const VideoListItem = ({ video }) => {
+const VideoListItem = ({ video, onVideoSelect }) => {
   console.log(video.snippet);
   const imageUrl = video.snippet.thumbnails.default.url;
   const videoTitle = video.snippet.title;
   console.log("[imageUrl]: " + imageUrl);
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl} />
