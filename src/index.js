9f439e8cb611458788b76ebbde3f84840c6d7d3f
Garett Gelsinger <garett.gelsinger@gmail.com>
2018-07-10 18:12:22 -0600

    refactor a bit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import YTSearch from "youtube-api-search";
 import ApiConfig from "./apiKeys";
+import _ from "lodash";
 
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
@@ -22,7 +23,6 @@ class App extends Component {
 
   videoSearch = term => {
     YTSearch({ key: API_KEY, term: term }, videos => {
-      //this.setstate({videos: videos});
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -31,9 +31,13 @@ class App extends Component {
   };
 
   render() {
+    const videoSearch = _.debounce(term => {
+      this.videoSearch(term);
+    }, 300);
+
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
